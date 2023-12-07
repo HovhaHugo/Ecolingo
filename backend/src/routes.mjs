80a57d6ce0b4060ef4baadf268952d70c863e48f
TSnake41 <TSnake@Outlook.fr>
2023-12-07 22:38:40 +0100

    Redesign /api/question

--- backend/src/routes.mjs
+++ backend/src/routes.mjs
@@ -1,17 +1,12 @@
+import * as fs from 'node:fs/promises';
+
+const data = JSON.parse(await fs.readFile("questions.json", "utf8")).data;
+
 /**
  * @param {import("express").Application} app 
  */
 export function makeRoutes(app) {
-    app.get("/api/question", (req, res) => {
-      const fs = require("fs");
-
-      fs.readFile("./questions.json", "utf8", (error, data) => {
-        if (error) {
-          console.log(error);
-          return;
-        }
-        let result = JSON.parse(data)
-        res.send(result.data[Math.floor(Math.random()*result.data.length)])
-      });
-    })
+  app.get("/api/question", async (req, res) => {
+    res.send(data[Math.floor(Math.random() * data.length)])
+  })
 }
