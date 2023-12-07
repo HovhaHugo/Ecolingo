f98aaeb3faac4a56c6f9c6efb750d7db399dcb2b
Bernard <74041418+BethencourtFlorian@users.noreply.github.com>
2023-12-07 22:32:20 +0100

    add JSON file

--- backend/src/routes.mjs
+++ backend/src/routes.mjs
@@ -2,11 +2,16 @@
  * @param {import("express").Application} app 
  */
 export function makeRoutes(app) {
-  app.get("/api/question", (req, res) => {
-    res.send({
-      question: "Lorem ipsum dolor sit amet",
-      responses: ["A", "B", "C", "D"],
-      valid: 0
+    app.get("/api/question", (req, res) => {
+      const fs = require("fs");
+
+      fs.readFile("./questions.json", "utf8", (error, data) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        let result = JSON.parse(data)
+        res.send(result.data[Math.floor(Math.random()*result.data.length)])
+      });
     })
-  })
 }
