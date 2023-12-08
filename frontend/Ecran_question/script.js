62ba530b8246ab3e32517c44addcc0c955fac042
Bernard <74041418+BethencourtFlorian@users.noreply.github.com>
2023-12-08 04:22:26 +0100

    ébauche appel API

---
+++ frontend/Ecran_question/script.js
@@ -0,0 +1,16 @@
+fetch("http://localhost:3000/api/question").then((response) => response.json()).then((json) => loadtext(json));
+
+const shuffle = (array) => { 
+    return array.sort(() => Math.random() - 0.5); 
+}; 
+
+function loadtext(question){
+    document.getElementById("question").textContent = question.Question
+    listRep = shuffle(["repA","repB","repC","repD"])
+       
+    document.getElementById(listRep[0]).textContent = question.A_true
+    document.getElementById(listRep[1]).textContent = question.B
+    document.getElementById(listRep[2]).textContent = question.C
+    document.getElementById(listRep[3]).textContent = question.D
+    //console.log(question)
+}
