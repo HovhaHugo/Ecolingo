9798304cca13d221b4b064b0a78513784ae4053b
Bernard <74041418+BethencourtFlorian@users.noreply.github.com>
2023-12-08 07:11:29 +0100

    fix

--- frontend/Ecran_question/script.js
+++ frontend/Ecran_question/script.js
@@ -22,7 +22,7 @@ function loadQuestion(question){
     listRep = shuffle(["repA","repB","repC","repD"])
 
     document.getElementById(listRep[0]).textContent = question.A_true
-    document.getElementById(listRep[0]).parentElement.parentElement.parentElement.classList += "reponse";
+    document.getElementById(listRep[0]).parentElement.parentElement.parentElement.classList += " reponse";
     document.getElementById(listRep[1]).textContent = question.B
     document.getElementById(listRep[2]).textContent = question.C
     document.getElementById(listRep[3]).textContent = question.D
